181e7c6809f546191d905e4e465096def26f691e
Ana Lazzari <ana.lazzari.ifpr@gmail.com>
2024-10-10 20:18:04 -0300

    Add files via upload

---
+++ 3Trim_20JavaScript/js/Ex01.js
@@ -0,0 +1,3 @@
+/* Prompt é um popup que permite a entrada de dados pelo usuário */
+prompt("Script JS em um arquivo externo")
+alert("Testando alert em um arquivo externo")
---
+++ 3Trim_20JavaScript/js/Ex04.js
@@ -0,0 +1,38 @@
+
+var n1, n2, subtracao
+
+n1=prompt("Insira um número: ")
+n2=prompt("Insira outro número: ")
+subtracao=n1-n2
+alert("o resultado da subtração é: " +subtracao)
+
+
+var n1, n2, n3, mult
+
+n1=prompt("Insira um número: ")
+n2=prompt("Insira outro número: ")
+n3=prompt("Insira o último número: ")
+mult=n1*n2*n3
+alert("O resultado da multilicação é: " +mult)
+
+
+var n1, cubo
+n1=prompt("Insira um número: ")
+cubo=n1*n1*n1
+alert("O cubo é: " +cubo)
+
+
+var n1, ant, sus
+n1=prompt("Informe um número: ")
+ant=n1-1
+sus=n1+1
+sus=parseInt(n1)+parseInt(1)
+alert("O antecessor é " +ant+ " e o sucessor é " +sus)
+
+
+var base, altura, area
+base=prompt("Insira o valor da base: ")
+altura=prompt("Insira o valor da altura: ")
+area=base*altura/2
+alert("A área do triângulo é: " +area)
+
